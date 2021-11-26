cd335b03d249d1dc27215eb7296cb6a53ffb9267
Phong Nguyen <ngt.phongg@gmail.com>
2021-11-26 21:07:37 +0700

    phan trang product

--- access/javascript/main.js
+++ access/javascript/main.js
@@ -21,3 +21,63 @@ function hideRegisterFormFnc() {
   document.getElementById("modal-id").classList.remove("show-component");
   document.getElementById("register-form-id").classList.remove("show-component");
 }
+
+function setProductType(typeID, type) {
+  this.typeID = typeID;
+  this.type = type;
+}
+
+function setProductItem(typeID, productID, name, price, sale) {
+  this.typeID = typeID;
+  this.productID = productID;
+  this.name = name;
+  this.price = price;
+  this.sale = sale;
+}
+
+// type list
+var arrType = [new setProductType("nike-id", "Nike"), new setProductType("adidas", "Adidas"), new setProductType("fila", "Fila"), new setProductType("champion", "Champion")];
+
+var arrProduct = [new setProductItem("nike-id", "nike-01", "nike 01", "1000$", "800$"), new setProductItem("nike-id", "nike-02", "nike 02", "2000$", "900$"), new setProductItem("nike-id", "nike-03", "nike 03", "3000$", "1000$"), new setProductItem("nike-id", "nike-04", "nike 04", "4000$", "1100$"), new setProductItem("nike-id", "nike-05", "nike 05", "5000$", "1200$"), new setProductItem("nike-id", "nike-06", "nike 06", "6000$", "1300$"), new setProductItem("nike-id", "nike-07", "nike 07", "7000$", "1400$"), new setProductItem("nike-id", "nike-08", "nike 08", "8000$", "1500$"), new setProductItem("nike-id", "nike-09", "Nike 09", "999$", "1000$")];
+
+// show product type list onload
+function showProductTypeListFnc() {
+  listType = "";
+
+  for (let i = 0; i < arrType.length; i++) {
+    var type = '<li class="product-type__item id="' + arrType[i].typeID + '" "><a href="" class="product-type__item-link">' + arrType[i].type + "</a></li>";
+    listType += type;
+  }
+
+  document.getElementById("product-type-list-id").innerHTML = listType;
+}
+
+// show home product list
+function showHomeProductList(pageNumber, maxIndex, arrProduct) {
+  var productRow = "";
+  var tempProductArr = [];
+  var max = pageNumber * maxIndex;
+  var k = pageNumber - 1;
+  var t = 0;
+
+  for (let i = 0; i < 3; i++) {
+    var productCols = "";
+    for (let j = 0; j < 3; j++) {
+      tempProductArr[t] = arrProduct[k];
+      var productCol = '<div class="grid__col-4">' + tempProductArr[t].name + "</div>";
+      productCols += productCol;
+      k++;
+      t++;
+    }
+    productRow += ' <div class="grid__row">' + productCols + "</div>";
+  }
+
+  document.getElementById("home-product-id").innerHTML = '<div class="grid">' + productRow + "</div>";
+}
+
+function onloadFnc() {
+  showProductTypeListFnc();
+  showHomeProductList(1, 9, arrProduct);
+}
+
+window.onload = onloadFnc;
